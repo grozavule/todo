8f100183f59230b3c57a3396aeab2f1802b13903
Eric Drake <puturosule@gmail.com>
2023-03-24 10:05:48 -0600

    added priority to the tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_24_034516) do
+ActiveRecord::Schema.define(version: 2023_03_24_155332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_03_24_034516) do
     t.text "description", null: false
     t.datetime "deadline", null: false
     t.datetime "date_completed"
+    t.integer "priority"
   end
 
 end
